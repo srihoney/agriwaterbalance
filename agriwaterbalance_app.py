95466ce5b614e8fd9c1a28b754855ce998524978
srihoney <sreeearth@gmail.com>
2025-04-07 14:42:41 -0700

    Update agriwaterbalance_app.py

--- agriwaterbalance_app.py
+++ agriwaterbalance_app.py
@@ -448,7 +448,7 @@ def fetch_weather_data(lat, lon, start_date, end_date, forecast=False, manual_da
 st.markdown("""
     <div class="header-container">
         <div class="header-logo">
-            <img src="logo.png" width="80">
+            <img src="logo.png" width="10">
         </div>
         <div class="header-title">
             AgriWaterBalance
